bcf58297bf71ede0c0a99e8d5c548fcc16fa1fbb
Romain BUREL <generic3@diademe.fr>
2024-08-24 18:27:39 +0200

    feat: implement Repository Pattern for data update - add GenericRepository + its interface - add CarRepository + its interface

---
+++ OCR_DotNet_P5_ExpressVoitures/Models/Repositories/CarRepository.cs
@@ -0,0 +1,17 @@
+﻿using OCR_DotNet_P5_ExpressVoitures.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace OCR_DotNet_P5_ExpressVoitures.Models.Repositories
+{
+    public class CarRepository : GenericRepository<Car>, ICarRepository
+    {
+        public CarRepository(DbContext context) : base(context)
+        {
+        }
+
+        public IEnumerable<Car> GetAllAvailable()
+        {
+            return this._dbSet.Where(c => c.NoMoreAvailable == false).ToList();
+        }
+    }
+}
