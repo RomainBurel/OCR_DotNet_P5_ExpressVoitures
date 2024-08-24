3172a4e0d133f947ac2461d240e0a61b6371d76c
Romain BUREL <generic3@diademe.fr>
2024-08-24 18:38:32 +0200

    feat: add CarsController - with all needed routes (Index, Details, DetailsForAdmin, Create, Delete, Update, Notification) - add CarService + its interface

---
+++ OCR_DotNet_P5_ExpressVoitures/Models/Services/ICarService.cs
@@ -0,0 +1,22 @@
+﻿using OCR_DotNet_P5_ExpressVoitures.Models.Entities;
+using OCR_DotNet_P5_ExpressVoitures.Models.ViewModels;
+
+namespace OCR_DotNet_P5_ExpressVoitures.Services
+{
+    public interface ICarService
+    {
+        public Car GetById(int id);
+
+        public CarModel GetCarModelbyId(int id);
+
+        public CarMainInfosModel GetCarMainInfosModelbyId(int id);
+
+        public IEnumerable<CarMainInfosModel> GetAll(bool onlyAvailableCars);
+
+        public void Add(CarModel carModel, IFormFile? image);
+
+        public void Update(CarModel carModel, IFormFile? image);
+
+        public void Delete(CarModel carModel);
+    }
+}
