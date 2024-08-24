f987bcf8e3adc60f45680340841a12d9db2a2036
Romain BUREL <generic3@diademe.fr>
2024-08-24 19:23:28 +0200

    feat: frontend with all views - add all needed views for CarsController (Create, Delete, Details, DetailsForAmin, Edit, Index, Notification) - update Home\Index view - update program.cs to enable CarService - update site css

---
+++ OCR_DotNet_P5_ExpressVoitures/Views/Shared/Components/ImageUploader/ImageUploaderViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OCR_DotNet_P5_ExpressVoitures.Views.Shared.Components.ImageUploader
+{
+    public class ImageUploaderViewComponent: ViewComponent
+    {
+        public ImageUploaderViewComponent()
+        {
+        }
+
+        public IViewComponentResult Invoke(string FieldName)
+        {
+            return View("Default", FieldName);
+        }
+    }
+}
