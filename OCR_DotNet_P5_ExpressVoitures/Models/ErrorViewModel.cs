f9643c4738c17162275cfb456498e40e45e5eebf
Romain BUREL <generic3@diademe.fr>
2024-08-24 16:39:19 +0200

    Ajoutez des fichiers projet.

---
+++ OCR_DotNet_P5_ExpressVoitures/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OCR_DotNet_P5_ExpressVoitures.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
