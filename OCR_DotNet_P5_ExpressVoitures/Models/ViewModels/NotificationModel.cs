9f7a892aa0115be3f0695a144d88ed77b5036be5
Romain BUREL <generic3@diademe.fr>
2024-08-24 18:32:09 +0200

    feat: add ViewModels - CarModel for futur views needing all Car data - CarMainInfosModel for futur views nedding only main car informations - NotificationModel for futur notification views

---
+++ OCR_DotNet_P5_ExpressVoitures/Models/ViewModels/NotificationModel.cs
@@ -0,0 +1,9 @@
+﻿namespace OCR_DotNet_P5_ExpressVoitures.Models.ViewModels
+{
+    public class NotificationModel
+    {
+        public string Title { get; set; }
+
+        public string Message { get; set; }
+    }
+}
